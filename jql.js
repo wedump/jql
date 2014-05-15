1b2f36ac0d5959a121cf0652c8b508abfc758678
wedump-ace <kwy2000@lycos.co.kr>
2014-05-15 15:41:59 +0900

    Applied foreach design pattern to _where function.

--- jql.js
+++ jql.js
@@ -20,7 +20,15 @@ var $jql = ( function() {
      }
 
      _utils = {
-
+          foreach : function( $roof, $nextFunction, $excuteContext, $keyFlag ) {
+               if ( $keyFlag ) {
+                    for ( var key in $roof )
+                         $nextFunction.apply( $excuteContext, [ key, $roof ] );
+               } else {
+                    for ( var i = 0; i < $roof.length; i++ )
+                         $nextFunction.apply( $excuteContext, [ i, $roof ] );
+               }
+          }
      };
 
      _createSql = function( $sqlFunction, $parameter ) {
@@ -47,8 +55,8 @@ var $jql = ( function() {
                var where = $sql.replace( /[^]+where\s+/, "" );
                var table = _storage[ tableName ];
                
-               // alias 방법에 대한 설계 필요.( 따로 분리 )
-               // var tableList = tableName.replace( /,/g, " " ).replace( /\s+/g, " " ).split( " " );
+               // * alias 방법에 대한 설계 필요.( 따로 분리 )
+               // * var tableList = tableName.replace( /,/g, " " ).replace( /\s+/g, " " ).split( " " );
 
                if ( !table ) throw new Error( "Not found table. -> " + tableName );
 
@@ -56,57 +64,71 @@ var $jql = ( function() {
           }
      };
 
-     // like, in, not in, is null, is not null, exists, not exists 처리 필요                    
+     // * like, in, not in, is null, is not null, exists, not exists 처리 필요
+     // * '''' 이스케이프 처리
      _where = function( $table, $where, $visitor, $customData ) {
           var result = [];
           var where  = $where.replace( /\s+and\s+/g, " && " ).replace( /\s+or\s+/g, " || " ).replace( /=/g, "==" );
 
-          for ( var i = 0; i < $table.length; i++ ) {
-               var row = $table[ i ];
-               var isExist = where;
+          var tableIterator = function( $index, $parent ) {
+               this.row = $parent[ $index ];
+               this.isExist = where;
 
-               for ( var col in row ) {
-                    var whereList = isExist.replace( /'/g, ":'" ).split( ":" );
-                    var quoteFlag = false;
-                    
-                    for ( var j = 0; j < whereList.length; j++ ) {
-                         var item = whereList[ j ], itemList;
+               this.foreach( this.row, rowIterator, true );
 
-                         if ( item.indexOf( "'" ) > -1 && !quoteFlag ) {
-                              quoteFlag = true;
-                              continue;
-                         }
-
-                         quoteFlag = false;
-                         itemList  = item.replace( new RegExp( col, "g" ), ":" + col + ":" ).split( ":" );
-
-                         for ( var k = 0; k < itemList.length; k++ ) {
-                              var subItem = itemList[ k ];
-                              var preSubItem  = itemList[ k - 1 ];
-                              var postSubItem = itemList[ k + 1 ];
-
-                              if ( subItem === col ) {
-                                   var preChar  = preSubItem.substr( preSubItem.length - 1, preSubItem.length );
-                                   var postChar = postSubItem.substr( 0, 1 );
-
-                                   if ( ( /\W/.test( preChar ) || !preChar ) && ( /\W/.test( postChar ) || !postChar ) )
-                                        itemList[ k ] = "'" + row[ col ] + "'";
-                              }
-                         }
-
-                         whereList[ j ] = itemList.join( "" );
-                    }
-
-                    isExist = whereList.join( "" );
+               if ( eval( this.isExist ) ) {
+                    result[ result.length ] = this.row;
+                    $visitor( $table, $index, $customData );
                }
+          };
+          var rowIterator = function( $key, $parent ) {
+               this.whereList = this.isExist.replace( /'/g, ":'" ).split( ":" );
+               this.quoteFlag = false;
+               this.col = $key;
 
-               if ( eval( isExist ) ) {
-                    result[ result.length ] = row;
-                    $visitor( $table, i, $customData );
+               this.foreach( this.whereList, whereIterator );
+
+               this.isExist = this.whereList.join( "" );
+          };
+          var whereIterator = function( $index, $parent ) {
+               this.item = $parent[ $index ];
+               this.itemList;
+
+               if ( this.item.indexOf( "'" ) > -1 && !this.quoteFlag ) {
+                    this.quoteFlag = true;
+               } else {
+                    this.quoteFlag = false;
+                    this.itemList  = this.item.replace( new RegExp( this.col, "g" ), ":" + this.col + ":" ).split( ":" );
+
+                    this.foreach( this.itemList, this.lastFunction );
+
+                    $parent[ $index ] = this.itemList.join( "" );
                }
-          }
+          };
+
+          _utils.foreach( $table, tableIterator, new Iterator( function( $index, $parent ) {
+               var subItem = $parent[ $index ];
+               var preSubItem  = $parent[ $index - 1 ];
+               var postSubItem = $parent[ $index + 1 ];
+
+               if ( subItem === this.col ) {
+                    var preChar  = preSubItem.substr( preSubItem.length - 1, preSubItem.length );
+                    var postChar = postSubItem.substr( 0, 1 );
+
+                    if ( ( /\W/.test( preChar ) || !preChar ) && ( /\W/.test( postChar ) || !postChar ) )
+                         $parent[ $index ] = "'" + this.row[ this.col ] + "'";
+               }
+          } ) );
 
           return result;
+     };
+
+     function Iterator( $lastFunction ) {
+          this.lastFunction = $lastFunction;
+     }
+
+     Iterator.prototype.foreach = function( $roof, $nextFunction, $keyFlag ) {
+          _utils.foreach( $roof, $nextFunction, this, $keyFlag );
      };
 
      function deleteVisitor( $table, $index, $customData ) {
