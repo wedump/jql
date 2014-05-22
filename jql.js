58f069084f46e4cf23be0409cecac675122ba2af
wedump-ace <kwy2000@lycos.co.kr>
2014-05-22 17:25:30 +0900

    Create _utils.replacePureWord, _core.alias function.

--- jql.js
+++ jql.js
@@ -1,11 +1,11 @@
 var $jql = ( function() {
      "use strict";
 
-     var _createSql, _player, _where, _utils, _storage = {};
+     var _core, _utils, _player, _storage = {};
 
      // argumets type : ( function, {} )
      function $jql( $sqlFunction, $parameter ) {
-          var sql = _createSql( $sqlFunction, $parameter );
+          var sql = _core.createSql( $sqlFunction, $parameter );
           var paragraph = sql.split( " " )[ 0 ]; // select, insert, update, delete, etc..
 
           return _player[ paragraph + "Excute" ]( sql );
@@ -32,8 +32,7 @@ var $jql = ( function() {
           getObjectLength : function( $object ) {
                var count = 0;
 
-               for ( var item in $object )
-                    count++;
+               for ( var item in $object ) count++;
 
                return count;
           },
@@ -46,16 +45,29 @@ var $jql = ( function() {
                }
 
                return index;
+          },
+          // 문자열에서 순수단어만을 찾아 치환
+          // 치환을 원하는 단어 : "as", "12as" -> X, "paas" -> X, "substr( name, 3 )as A" -> O
+          replacePureWord : function( $target, $searchWord, $division ) {
+               var replaceWord = $division + $searchWord + $division;
+               var targetSplit = $target.replace( new RegExp( $searchWord, "g" ), replaceWord ).split( $division );
+
+               for ( var i = 0; i < targetSplit.length; i++ ) {
+                    var item     = targetSplit[ i ];                  
+                    var preItem  = targetSplit[ i - 1 ];
+                    var postItem = targetSplit[ i + 1 ];
+
+                    if ( item === $searchWord ) {
+                         var preChar  = preItem.substr( preItem.length - 1, preItem.length );
+                         var postChar = postItem.substr( 0, 1 );
+                         
+                         if ( ( /\W/.test( preChar ) || !preChar ) && ( /\W/.test( postChar ) || !postChar ) )
+                              targetSplit[ i ] = replaceWord;
+                    }
+               }
+
+               return targetSplit.join( "" );
           }
-     };
-
-     _createSql = function( $sqlFunction, $parameter ) {
-          var sql = $sqlFunction.toString().replace( /^[^]+\/\*\s*|\s*\*\/[^]*/g, "" ).toLowerCase();
-
-          for ( key in $parameter )
-               sql.replace( new RegExp( ":" + key, [ "g" ] ), "'" + $parameter[ key ] + "'" );
-
-          return sql;
      };
 
      _player = {
@@ -71,73 +83,89 @@ var $jql = ( function() {
           deleteExcute : function( $sql ) {
                var tableName = $sql.replace( /\s*delete\s+from\s+|\s+where[^]*/g, "" );
                var where = $sql.replace( /[^]+where\s+/, "" );
-               var table = _storage[ tableName ];
+               var table = _storage[ _core.alias( tableName ).tableName ];
 
                if ( !table ) throw new Error( "Not found table. -> " + tableName );
 
-               return _where( table, where, deleteVisitor, null ).length;
+               return _core.where( table, where, deleteVisitor, null ).length;
           }
      };
 
-     _where = function( $table, $where, $visitor, $customData ) {
-          var result = [];
+     _core = {
+          createSql : function( $sqlFunction, $parameter ) {
+               var sql = $sqlFunction.toString().replace( /^[^]+\/\*\s*|\s*\*\/[^]*/g, "" ).toLowerCase();
 
-          var tableIterator = function( $index, $parent ) {
-               this.row = $parent[ $index ];
-               this.isExist = $where;
+               for ( key in $parameter )
+                    sql.replace( new RegExp( ":" + key, [ "g" ] ), "'" + $parameter[ key ] + "'" );
 
-               this.foreach( this.row, rowIterator, true );
+               return sql;
+          },
+          where : function( $table, $where, $visitor, $customData ) {
+               var result = [];
 
-               if ( eval( this.isExist ) ) {
-                    result[ result.length ] = this.row;
-                    $visitor( $table, $index, $customData );
-               }
-          };
-          var rowIterator = function( $key, $parent ) {
-               this.whereList = this.isExist.replace( /'/g, ":'" ).split( ":" ); // 문자열 내부의 문자를 제외하고 컬럼을 대입하기 위함.
-               this.quoteFlag = false;
-               this.col = $key;
+               var tableIterator = function( $index, $parent ) {
+                    this.row = $parent[ $index ];
+                    this.isExist = $where;
 
-               this.foreach( this.whereList, whereIterator );
+                    this.foreach( this.row, rowIterator, true );
 
-               this.isExist = this.whereList.join( "" );
-          };
-          var whereIterator = function( $index, $parent ) {
-               this.item = $parent[ $index ];
-               this.itemList;
-
-               if ( this.item.indexOf( "'" ) > -1 && !this.quoteFlag ) {
-                    this.quoteFlag = true;
-               } else {
-                    // 마지막에 한번만 교체 되어야 하는 문자 처리
-                    if ( _utils.getObjectLength( this.row ) - 1 === _utils.getColumnIndex( this.row, this.col ) )
-                         this.item = this.item.replace( /\s+and\s+/g, " && " ).replace( /\s+or\s+/g, " || " ).replace( /=/g, "==" );
-
+                    if ( eval( this.isExist ) ) {
+                         result[ result.length ] = this.row;
+                         $visitor( $table, $index, $customData );
+                    }
+               };
+               var rowIterator = function( $key, $parent ) {
+                    this.whereList = this.isExist.replace( /'/g, ":'" ).split( ":" ); // 문자열 내부의 문자를 제외하고 컬럼을 대입하기 위함.
                     this.quoteFlag = false;
-                    this.itemList  = this.item.replace( new RegExp( this.col, "g" ), ":" + this.col + ":" ).split( ":" ); // 컬럼 문자의 앞뒤 문자를 확인 위함.
+                    this.col = $key;
 
-                    this.foreach( this.itemList, this.lastFunction );
+                    this.foreach( this.whereList, whereIterator );
 
-                    $parent[ $index ] = this.itemList.join( "" );
-               }
-          };
+                    this.isExist = this.whereList.join( "" );
+               };
+               var whereIterator = function( $index, $parent ) {
+                    this.item = $parent[ $index ];
+                    this.itemList;
 
-          _utils.foreach( $table, tableIterator, new Iterator( function( $index, $parent ) {
-               var subItem = $parent[ $index ];
-               var preSubItem  = $parent[ $index - 1 ];
-               var postSubItem = $parent[ $index + 1 ];
+                    if ( this.item.indexOf( "'" ) > -1 && !this.quoteFlag ) {
+                         this.quoteFlag = true;
+                    } else {
+                         // 마지막에 한번만 교체 되어야 하는 문자 처리
+                         if ( _utils.getObjectLength( this.row ) - 1 === _utils.getColumnIndex( this.row, this.col ) )
+                              this.item = this.item.replace( /\s+and\s+/g, " && " ).replace( /\s+or\s+/g, " || " ).replace( /=/g, "==" );
 
-               if ( subItem === this.col ) {
-                    var preChar  = preSubItem.substr( preSubItem.length - 1, preSubItem.length );
-                    var postChar = postSubItem.substr( 0, 1 );
+                         this.quoteFlag = false;
+                         this.itemList  = this.item.replace( new RegExp( this.col, "g" ), ":" + this.col + ":" ).split( ":" ); // 컬럼 문자의 앞뒤 문자를 확인 위함.
 
-                    // 컬럼 문자의 앞뒤 문자가 영문자 또는 숫자가 아니어야 컬럼 문자가 포함된 문자열에 속지 않음.
-                    if ( ( /\W/.test( preChar ) || !preChar ) && ( /\W/.test( postChar ) || !postChar ) )
-                         $parent[ $index ] = "'" + this.row[ this.col ] + "'";
-               }
-          } ) );
+                         this.foreach( this.itemList, this.lastFunction );
 
-          return result;
+                         $parent[ $index ] = this.itemList.join( "" );
+                    }
+               };
+
+               _utils.foreach( $table, tableIterator, new Iterator( function( $index, $parent ) {
+                    var subItem = $parent[ $index ];
+                    var preSubItem  = $parent[ $index - 1 ];
+                    var postSubItem = $parent[ $index + 1 ];
+
+                    if ( subItem === this.col ) {
+                         var preChar  = preSubItem.substr( preSubItem.length - 1, preSubItem.length );
+                         var postChar = postSubItem.substr( 0, 1 );
+
+                         // 컬럼 문자의 앞뒤 문자가 영문자 또는 숫자가 아니어야 컬럼 문자가 포함된 문자열에 속지 않음.
+                         if ( ( /\W/.test( preChar ) || !preChar ) && ( /\W/.test( postChar ) || !postChar ) )
+                              $parent[ $index ] = "'" + this.row[ this.col ] + "'";
+                    }
+               } ) );
+
+               return result;
+          },
+          // argument type : 컬럼 리스트나 테이블 리스트 중 한 줄만 문자열로 들어옴
+          alias : function( $string ) {
+               var stringList = _utils.replacePureWord( $string, "as", ":" ).split( ":as:" );
+
+               return { tableName : stringList[ 0 ], alias : stringList[ 1 ] };
+          }
      };
 
      function Iterator( $lastFunction ) {
